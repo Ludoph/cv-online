8c1f031ecd7cc8fa413cceb5ff0b5c50426aa52d
Ludoph <129503345+Ludoph@users.noreply.github.com>
2023-05-15 17:02:51 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,22 @@
 const menuBurger = document.querySelector(".menu-burger");//selection du bouton menu
 const navLinks = document.querySelector(".nav-links");//selection de l'affichage du menu
 const visit = document.querySelector('#visit');//selection du block vues
+
+const menuLinks = document.querySelector('.btn-logo');//selection du bouton de la photo
+const showLinks = document.querySelector('.links');//selection de la section réseaux 
+
+//selection des card-formations en bouton
+const btnInfoCard1 = document.querySelector('.btn-info-card1');
+const btnInfoCard2 = document.querySelector('.btn-info-card2');
+const btnInfoCard3 = document.querySelector('.btn-info-card3');
+const btnInfoCard4 = document.querySelector('.btn-info-card4');
+//selection des bloc infos formations
+const infoCard1 = document.querySelector('.info-card1');
+const infoCard2 = document.querySelector('.info-card2');
+const infoCard3 = document.querySelector('.info-card3');
+const infoCard4 = document.querySelector('.info-card4');
+
+
 menuBurger.addEventListener('click',()=>{
     navLinks.classList.toggle('mobile-menu');
     //afficher cacher le compteur de vues quand le nav link est affiché ou cahcé
@@ -20,12 +36,10 @@ navLinks.addEventListener('click', () => {
     } else {
         visit.style.display = 'block';
     }
+
 });
 
 // Afficher / Masquer les réseaux
-const menuLinks = document.querySelector('.btn-logo');
-const showLinks = document.querySelector('.links');
-
 menuLinks.addEventListener('click', () =>{
     showLinks.classList.toggle('mobile-links');
 });
@@ -33,16 +47,8 @@ showLinks.addEventListener('click', () =>{
     showLinks.classList.toggle('mobile-links');
 });
 
-//affichier / Masquer contenu des cards 
-const btnInfoCard1 = document.querySelector('.btn-info-card1');
-const btnInfoCard2 = document.querySelector('.btn-info-card2');
-const btnInfoCard3 = document.querySelector('.btn-info-card3');
-const btnInfoCard4 = document.querySelector('.btn-info-card4');
-const infoCard1 = document.querySelector('.info-card1');
-const infoCard2 = document.querySelector('.info-card2');
-const infoCard3 = document.querySelector('.info-card3');
-const infoCard4 = document.querySelector('.info-card4');
 
+//afficher ou masquer les infos formations
 btnInfoCard1.addEventListener('click', () =>{
     if(infoCard1.style.display == 'flex'){
         infoCard1.style.display = 'none';
@@ -74,10 +80,10 @@ btnInfoCard4.addEventListener('click', () =>{
 
 //Afichier cacher les sections
 function showProfil(){
-    document.getElementById('profil').style.display = "flex";
-    document.getElementById('competences').style.display = "none";
-    document.getElementById('formations').style.display = "none";
-    document.getElementById('projets').style.display = "none"
+    document.getElementById('profil').style.display = "flex";//visible
+    document.getElementById('competences').style.display = "none";//caché
+    document.getElementById('formations').style.display = "none";//caché
+    document.getElementById('projets').style.display = "none"//caché
 }
 function showCompetences(){
     document.getElementById('profil').style.display = "none";
@@ -105,9 +111,9 @@ const visitCountElement = document.getElementById('visit-count');
 visitCountElement.textContent = visitCounter;
 
 function incrementVisitCounter() {
-  const newVisitCounter = parseInt(visitCounter) + 1;
+  const newVisitCounter = parseInt(visitCounter) + 1;// ajoute +1 
   localStorage.setItem('visitCounter', newVisitCounter);
-  visitCountElement.textContent = newVisitCounter;
+  visitCountElement.textContent = newVisitCounter;//affiche le resultat
 }
 
-window.onload = incrementVisitCounter;
+window.onload = incrementVisitCounter;//incrémentation lors d'un chargement de page affiché
